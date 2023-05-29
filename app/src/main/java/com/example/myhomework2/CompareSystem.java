3862eab5df3ef64eed2e32d412979b3f5cf4896b
Your Name <email@example.com>
2023-05-29 20:00:52 +0800

    Change the Homework to another hub

---
+++ app/src/main/java/com/example/myhomework2/CompareSystem.java
@@ -0,0 +1,14 @@
+package com.example.myhomework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompareSystem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compare_system);
+    }
+}
